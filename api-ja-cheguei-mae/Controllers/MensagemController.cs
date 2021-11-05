9a2d7d6fe0da88d02d7c2715b74e4a33bfeff18d
Roberto <robertocpaes@gmail.com>
2021-11-05 20:05:20 -0300

    iniciando repo principal

---
+++ api-ja-cheguei-mae/Controllers/MensagemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_ja_cheguei_mae.Controllers
+{
+    public class MensagemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
